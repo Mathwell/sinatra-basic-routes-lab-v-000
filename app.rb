72d0f1988d2da864d134e3d49fc6eb6f777bc4ca
Olena Ageyeva <eageeva@hotmail.com>
2017-12-10 20:08:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "Olena"
+  end
+  get '/hometown' do
+  "Bedford"
+  end
+  get '/favorite-song' do
+  "Christmas"
+  end
 end
